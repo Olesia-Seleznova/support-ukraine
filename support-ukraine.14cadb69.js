000f91c50ebf2fd63df86bf18ba2a1d0fc6a37b4
Olesia-Seleznova <106627666+Olesia-Seleznova@users.noreply.github.com>
2023-08-05 19:42:16 +0000

    Deploying to gh-pages from @ Olesia-Seleznova/support-ukraine@359ae5d5725cfa6b20a26edb23c161a7fb7b26dc 🚀

---
+++ support-ukraine.14cadb69.js
@@ -0,0 +1,2 @@
+var refs={supportUkraine:document.querySelector(".support-ukraine"),funds:document.querySelector(".funds"),slider:document.querySelector(".slider")};function onSliderClick(){fundsList.classList.toggle("active"),refs.supportUkraine.classList.toggle("active")}refs.slider.addEventListener("click",onSliderClick);
+//# sourceMappingURL=support-ukraine.14cadb69.js.map
--- support-ukraine.7fba3a7b.js
+++
@@ -1,2 +0,0 @@
-var refs={funds:document.querySelector(".funds"),slider:document.querySelector(".slider")};function onSliderClick(){fundsList.classList.toggle("active")}refs.slider.addEventListener("click",onSliderClick);
-//# sourceMappingURL=support-ukraine.7fba3a7b.js.map
